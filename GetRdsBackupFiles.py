da3f3b534809e4b94b28dc2405c8d0fc0a2f58c4
zhuhong.Lee <lizhuhongno1@163.com>
2019-11-04 18:17:27 +0800

    upload

---
+++ GetRdsBackupFiles.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import re
+import os
+import wget
+import argparse
+import schedule
+import time
+import math
+from datetime import datetime, date, timedelta
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkrds.request.v20140815.DescribeBackupsRequest import DescribeBackupsRequest
+from retrying import retry
+
+
+def parse_args(AccessKeyId, AccessKeySecret):
+    # 定义默认起止时间
+    start_time = (date.today() + timedelta(days=-7)).strftime("%Y-%m-%dT00:00Z")
+    end_time = datetime.now().strftime("%Y-%m-%dT%H:%MZ")
+
+    # 解析入参
+    parser = argparse.ArgumentParser(usage='%(prog)s [options]')
+    parser.add_argument('-i', metavar='--instanceid', help='DB InstanceId')
+    parser.add_argument('-st', default=start_time, metavar='--start-time', help='Backup Start Time. eg:2019-09-10T00:00Z')
+    parser.add_argument('-et', default=end_time, metavar='--end-time', help='Backup End Time. eg:2019-09-10T00:00Z')
+    parser.add_argument('-d', default='backup', metavar='--directory', help='create dir if not exists on current dir,default dirname=backup ')
+    args = parser.parse_args()
+    if args.i and args.st and args.et and args.d:
+        downbakupfile(AccessKeyId, AccessKeySecret, args)
+    else:
+        print("请输入DB实例参数，eg: -i rm-uf68xxxxxxxxxx")
+
+
+@retry
+def downbakupfile(AccessKeyId, AccessKeySecret, args):
+    client = AcsClient(AccessKeyId, AccessKeySecret, 'cn-shanghai', timeout=600)
+    start_time = args.st
+    end_time = args.et
+
+    request = DescribeBackupsRequest()
+    request.set_accept_format('json')
+
+    request.set_DBInstanceId(args.i)
+
+    # 新建目录并下载备份文件
+    if os.path.exists(args.d):
+        pass
+    else:
+        os.mkdir(args.d)
+
+    # 获取备份总数及每页显示数，计算页数
+    request.set_StartTime(start_time)
+    request.set_EndTime(end_time)
+    response = client.do_action_with_exception(request)
+    pages = math.ceil(eval(str(response, encoding='utf-8'))['TotalRecordCount'] / eval(str(response, encoding='utf-8'))['PageRecordCount'])
+
+    for page in range(1, pages + 1):
+        request.set_PageNumber(page)
+        response = client.do_action_with_exception(request)
+        backupdetail = eval(str(response, encoding='utf-8'))['Items']['Backup']
+
+        for i in range(len(backupdetail)):
+            bakfile_url = backupdetail[i]['BackupDownloadURL']  # 外网下载地址
+            # bakfile_url = backupdetail[i]['BackupIntranetDownloadURL']   # 内网下载地址
+            re_result = re.findall("hins(.*).tar.gz", backupdetail[i]['BackupDownloadURL'])[0]  # 内网下载地址
+            # re_result = re.findall("hins(.*).tar.gz", backupdetail[i]['BackupIntranetDownloadURL'])[0]   # 外网下载地址
+            bakfile = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), args.d), 'hins' + re_result + '.tar.gz')
+            if os.path.exists(bakfile):
+                pass
+            else:
+                wget.download(bakfile_url, out=bakfile)
+                print('下载文件 %s 成功' % bakfile)
+
+    deletefile(args.d)
+
+
+## 默认删除30天之前的文件
+def deletefile(dir):
+    path = os.path.join(os.getcwd(), dir)
+    for eachfile in os.listdir(path):
+        filename = os.path.join(path, eachfile)
+        if os.path.isfile(filename):
+            lastmodifytime = os.stat(filename).st_mtime
+            endfiletime = time.time() - 3600 * 24 * 30
+            if endfiletime > lastmodifytime:
+                if filename[-7:] == ".tar.gz":  # 删除后缀为 .tar.gz 的文件
+                    os.remove(filename)
+                    print("删除文件 %s 成功" % filename)
+        elif os.path.isdir(filename):  # 如果是目录则递归调用当前函数
+            deletefile(filename)
+
+
+def main():
+    AccessKeyId = 'xxxxxxxxxxxxxxxxxxxx'
+    AccessKeySecret = 'xxxxxxxxxxxxxxxxxxxx'
+    parse_args(AccessKeyId, AccessKeySecret)
+
+
+if __name__ == '__main__':
+    # main()
+    schedule.every(30).minutes.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(600)
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # get_rds_backup
+
+包含下载全库备份文件 GetRdsBackupFiles.py 及binlog文件 GetRdsBackupLogFiles.py
+
+### 安装依赖包
+pip install aliyun-python-sdk-core aliyun-python-sdk-rds wget retrying
+
+
+### 新建阿里云账号，修改代码中的 AccessKey和SecretKey
+授权如下:
+
+{
+  "Statement": [
+    {
+      "Action": "rds:Describe*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ],
+  "Version": "1"
+}
+
+
+{
+  "Version": "1",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "rds:ModifyBackupPolicy"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+
+### 使用方法
+
+###### 1.下载全库备份文件
+optional arguments:  
+  -h, --help        show this help message and exit  
+  -i --instanceid   ，必填，如：rm-ufm6xxxxxxxxxx   
+  -st --start-time  查看某个时间段的备份文件，此为开始时间，eg:2019-09-10T00:00Z，默认 now()-7 days  
+  -et --end-time    同上，此为结束时间，eg:2019-09-10T00:00Z，默认 now()  
+  -d --directory    在当前执行目录创建备份目录，默认文件夹名：backup  
+
+
+示例：
+D:\开发\SVN Upload\get_rds_backup>python GetRdsBackupFiles.py -i rm-uf68xxxxxxxxxx -d crm -st 2019-09-10T00:00Z -et 2019-11-03T00:00Z  
+100% [......................................................................] 189379082 / 189379082下载文件 D:\开发\SVN Upload\get_rds_backup\crm\hins7510443_data_20191104065245.tar.gz 成功  
+  2% [.                                                                     ]   4644864 / 188754998
+
+
+###### 2.下载binlog文件
+optional arguments:  
+  -h, --help        show this help message and exit  
+  -i --instanceid   InstanceId，必填，如：rm-ufm6xxxxxxxxxx  
+  -st --start-time  查看某个时间段的备份日志，此为开始时间，eg:2019-09-10T00:00:00Z，默认 now()-7 days  
+  -et --end-time    同上，此为结束时间，eg:2019-09-10T00:00:00Z，默认 now()  
+  -d --directory    在当前执行目录创建备份目录，默认文件夹名：backup  
+
+
+示例：  
+D:\开发\SVN Upload\get_rds_backup>python GetRdsBackupLogFiles.py -i rm-uf68xxxxxxxxxx -d crm -st 2019-09-10T00:00:00Z -et 2019-11-03T00:00:00Z  
+100% [......................................................................] 174040615 / 174040615下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510443_mysql-bin.000794 成功  
+100% [..........................................................................] 6590223 / 6590223下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510443_mysql-bin.000793 成功  
+100% [..........................................................................] 6649732 / 6649732下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510443_mysql-bin.000792 成功  
+100% [..........................................................................] 6585247 / 6585247下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510443_mysql-bin.000791 成功  
+100% [..........................................................................] 6598934 / 6598934下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510443_mysql-bin.000790 成功  
+100% [..........................................................................] 6716054 / 6716054下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510443_mysql-bin.000789 成功  
+100% [..........................................................................] 6591846 / 6591846下载文件 D:\开发\SVN Upload\get_rds_backup\crm\7510441_mysql-bin.000788 成功  
+ 33% [.......................                                                 ] 29450240 / 88473876
